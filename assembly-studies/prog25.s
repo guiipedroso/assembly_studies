19da500dbc5111be5d1e0436b4e76023c4bcd58f
Guilherme Pedroso <82010727+guiipedroso@users.noreply.github.com>
2022-09-26 20:36:21 -0400

    Assembly studies

---
+++ assembly-studies/prog25.s
@@ -0,0 +1,18 @@
+.section .data
+  values:
+    .int 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60
+.section .text
+  .globl _start 
+  _start:
+    nop 
+    movl values, %eax 
+    
+    movl $values, %edi
+    movl $100, 12(%edi)
+    
+    movl $3, %edi
+    movl values(, %edi, 4), %ebx
+
+    movl $1, %eax
+    movl $0, %ebx
+    int $0x80
